9627a5ffd776e26a5787788275271927254c607c
Satish Singh <satish.kx@gmail.com.com>
2024-10-03 13:47:02 +0100

    added rest end points

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,19 +7,21 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
+	"github.com/omsatish/simplebank/db/util"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+
+	config, err := util.LoadConfig("../..")
+
+	if err != nil {
+		log.Fatal("Cannot load config file")
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
